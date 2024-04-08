4c5fed7509653bd12ca74a5e184b1efbf6a5aaf9
Bhakti12 <sanghanibhakti922@gmail.com>
2024-04-08 12:56:27 +0530

    post methods for add careplan_pathway and formtypes_pathway is ready

--- src/controllers/pathwayScenarioController.ts
+++ src/controllers/pathwayScenarioController.ts
@@ -1,4 +1,5 @@
 import { IPathwayscenarioService } from "../interfaces/IPathwayscenarioService";
+import { careplan_pathway, formtypes_pathway } from "../types/Pathwayscenario";
 import BaseController from "./BaseController";
 import express from "express";
 export default class PathwayScenarioController extends BaseController {
@@ -9,13 +10,43 @@ export default class PathwayScenarioController extends BaseController {
     this._pathwayService = pathwayService;
   }
 
-  async addPathway_formtypes(req:express.Request,res:express.Response): Promise<any> {
+  async addPathway_formtypes(
+    req: express.Request,
+    res: express.Response
+  ): Promise<any> {
     try {
-    } catch (err) {}
+      const pathway_formtypes: formtypes_pathway = {
+        resourceType: req.body.resourceType,
+        pathway_id: req.body.pathway_id,
+        formtype_id: req.body.formtype_id,
+      };
+      const result = await this._pathwayService.addPathway_formtypes(
+        pathway_formtypes
+      );
+      this.sendJSONResponse(res, "added formtype pathway", { data: 1 }, result);
+    } catch (err) {
+      console.log(err);
+      this.sendErrorResponse(req, res, err);
+    }
   }
 
-  async careplan_pathway(req:express.Request,res:express.Response): Promise<any> {
+  async careplan_pathway(
+    req: express.Request,
+    res: express.Response
+  ): Promise<any> {
     try {
-    } catch (err) {}
+      const careplan_pathway: careplan_pathway = {
+        resourceType: req.body.resourceType,
+        pathway_id: req.body.pathway_id,
+        careplan_id: req.body.careplan_id,
+      };
+      const result = await this._pathwayService.addCareplan_pathway(
+        careplan_pathway
+      );
+      this.sendJSONResponse(res, "added careplan pathway", { data: 1 }, result);
+    } catch (err) {
+      console.log(err);
+      this.sendErrorResponse(req, res, err);
+    }
   }
 }
--- src/types/Pathwayscenario.ts
+++ src/types/Pathwayscenario.ts
@@ -1,9 +1,11 @@
 export declare type careplan_pathway = {
+    resourceType: string;
     pathway_id: string;
     careplan_id: string;
-}
+};
 
 export declare type formtypes_pathway = {
+    resourceType: string;
     pathway_id: string;
     formtype_id: string;
-}
+};
